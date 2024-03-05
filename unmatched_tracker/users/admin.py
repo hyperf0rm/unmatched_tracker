ccbb1cdc32bca5d7365891341f263e2aec567ee3
Дмитрий Николайчик <drk2701@gmail.com>
2024-03-05 22:32:31 +0300

    Initial commit

---
+++ unmatched_tracker/users/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    pass
