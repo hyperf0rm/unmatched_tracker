b0dc8665fdd66638399a14be26fb0bd66766792c
Дмитрий Николайчик <drk2701@gmail.com>
2024-03-06 02:21:13 +0300

    add hero and sidekick models

--- unmatched_tracker/games/admin.py
+++ unmatched_tracker/games/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from games.models import Hero, Sidekick
+
+
+class SidekickInline(admin.TabularInline):
+    model = Sidekick
+    extra = 1
+
+
+@admin.register(Hero)
+class HeroAdmin(admin.ModelAdmin):
+    inlines = (SidekickInline,)
+
+
+@admin.register(Sidekick)
+class SidekickAdmin(admin.ModelAdmin):
+    pass
--- unmatched_tracker/games/models.py
+++ unmatched_tracker/games/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from games.constants import MAX_NAME_LENGTH, STR_LIMIT, GENERAL_CHAR_LENGTH
+
+
+class Character(models.Model):
+    ATTACK_TYPES = (
+        ('MELEE', 'Melee'),
+        ('RANGED', 'Ranged')
+    )
+    name = models.CharField(max_length=MAX_NAME_LENGTH)
+    attack_type = models.CharField(max_length=GENERAL_CHAR_LENGTH,
+                                   choices=ATTACK_TYPES)
+    speed = models.PositiveSmallIntegerField(default=2)
+
+    class Meta:
+        ordering = ('name',)
+        abstract = True
+
+    def __str__(self) -> str:
+        return self.name[:STR_LIMIT]
+
+
+class Hero(Character):
+    about = models.TextField(blank=True)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(blank=True)
+    ability = models.TextField(blank=True)
+
+
+class Sidekick(Character):
+    hero = models.ForeignKey(Hero,
+                             on_delete=models.CASCADE,
+                             related_name='sidekicks')
+    count = models.PositiveSmallIntegerField(default=0)
